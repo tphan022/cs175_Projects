52153c0d1feee782bd295b8c4c789c9514adc2c4
Tony Phan <tphan002@ucr.edu>
2017-11-01 17:32:14 +0000

    Finished solution check function and swap functions

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -10,6 +10,8 @@ struct eight_node {
     int board[3][3];
     //int cost;
     //int depth;
+    int solution[3][3];
+    bool valid;
     eight_node *left;
     eight_node *up;
     eight_node *right;
@@ -21,21 +23,164 @@ struct eight_node {
         up = NULL;
         right = NULL;
         down = NULL;
+        int k = 1;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                solution[i][j] = k;
+                k++;
+            }
+        }
+        solution[2][2] = 0;
     }
-    // eight_node operator=(eight_node rgt) {
-    //     for(int i = 0; i < 3; i++) {
-    //         for(int j = 0; j < 3; j++) {
-    //             this->board[i][j] = rgt.board[i][j];
-    //         }
-    //     }
-    //     this->left = rgt.left;
-    //     this->right = rgt.right;
-    //     this->up = rgt.up;
-    //     this->down = rgt.down;
-    //     return *this;
-    // }
-    void generate() {
+    eight_node* generate_left() {
+        eight_node *gen = new eight_node; // generates a new node
+        int row = -1;
+        int column = -1;
+        for(int i = 0; i < 3; ++i) {
+            for(int j = 0; j < 3; ++j) {
+                if(board[i][j] == 0) {
+                    row = i;
+                    column = j;
+                    //std::cout << "\n" << i << "\n" << j << "\n";
+                }
+            }
+        }
+        if(column > 0) {
+            gen->board[row][column] = this->board[row][column - 1];
+            gen->board[row][column - 1] = this->board[row][column];
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    if( (row == i && column == j) || (row == i && (column-1) == j ) ) {
+                        
+                    }
+                    else {
+                        gen->board[i][j] = this->board[i][j];
+                    }
+                }
+            }
+            gen->valid = true;
+        }
+        else {
+            gen->valid = false;
+        }
+        return gen;
         
+    }
+    
+    eight_node* generate_right() {
+        eight_node *gen = new eight_node; // generates a new node
+        int row = -1;
+        int column = -1;
+        for(int i = 0; i < 3; ++i) {
+            for(int j = 0; j < 3; ++j) {
+                if(board[i][j] == 0) {
+                    row = i;
+                    column = j;
+                    //std::cout << "\n" << i << "\n" << j << "\n";
+                }
+            }
+        }
+        if(column < 2) {
+            gen->board[row][column] = this->board[row][column + 1];
+            gen->board[row][column + 1] = this->board[row][column];
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    if( (row == i && column == j) || (row == i && (column+1) == j ) ) {
+                        
+                    }
+                    else {
+                        gen->board[i][j] = this->board[i][j];
+                    }
+                }
+            }
+            gen->valid = true;
+        }
+        else {
+            gen->valid = false;
+        }
+        return gen;
+        
+    }
+    
+    eight_node* generate_down() {
+        eight_node *gen = new eight_node; // generates a new node
+        int row = -1;
+        int column = -1;
+        for(int i = 0; i < 3; ++i) {
+            for(int j = 0; j < 3; ++j) {
+                if(board[i][j] == 0) {
+                    row = i;
+                    column = j;
+                    //std::cout << "\n" << i << "\n" << j << "\n";
+                }
+            }
+        }
+        if(row < 2) {
+            gen->board[row][column] = this->board[row + 1][column];
+            gen->board[row + 1][column] = this->board[row][column];
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    if( (row == i && column == j) || ( (row+1) == i && column == j ) ) {
+                        
+                    }
+                    else {
+                        gen->board[i][j] = this->board[i][j];
+                    }
+                }
+            }
+            gen->valid = true;
+        }
+        else {
+            gen->valid = false;
+        }
+        return gen;
+        
+    }
+    
+    eight_node* generate_up() {
+        eight_node *gen = new eight_node; // generates a new node
+        int row = -1;
+        int column = -1;
+        for(int i = 0; i < 3; ++i) {
+            for(int j = 0; j < 3; ++j) {
+                if(board[i][j] == 0) {
+                    row = i;
+                    column = j;
+                    //std::cout << "\n" << i << "\n" << j << "\n";
+                }
+            }
+        }
+        if(row > 0) {
+            gen->board[row][column] = this->board[row - 1][column];
+            gen->board[row - 1][column] = this->board[row][column];
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    if( (row == i && column == j) || ( (row-1) == i && column == j ) ) {
+                        
+                    }
+                    else {
+                        gen->board[i][j] = this->board[i][j];
+                    }
+                }
+            }
+            gen->valid = true;
+        }
+        else {
+            gen->valid = false;
+        }
+        return gen;
+        
+    }
+    
+    bool check_solution() {
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(board[i][j] != solution[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
     
 };
@@ -64,6 +209,8 @@ bool checklist(std::list<eight_node> ls, eight_node chk) {
 void UCS(eight_node root) { // Uniform Cost Search
     std::queue<eight_node> QF;
     std::list<eight_node> LS;
+    eight_node curr;
+    QF.push(root);
     // int g = 0;
     // int arr2[9] = {1,2,3,4,0,6,7,5,8};
     // eight_node test;
@@ -73,8 +220,10 @@ void UCS(eight_node root) { // Uniform Cost Search
     //         g++;
     //     }
     // }
-    eight_node curr;
-    QF.push(root);
+    while(!QF.empty()) {
+        curr = QF.front();
+        QF.pop();
+    }
     curr = QF.front();
     // LS.push_back(test);
     // checklist(LS, curr);
@@ -85,6 +234,7 @@ void UCS(eight_node root) { // Uniform Cost Search
 
 int main() {
     eight_node B;
+    eight_node *C;
     int arr[9] = {1,2,3,4,0,6,7,5,8};
     int k = 0;
     for(int i = 0; i < 3; i++) {
@@ -99,7 +249,15 @@ int main() {
         }
         std::cout << "\n";
     }
-    UCS(B);
+    std::cout << "\n" << B.check_solution() << "\n";
+    // C = B.generate_up();
+    // for(int i = 0; i < 3; i++) {
+    //     for(int j = 0; j < 3; j++) {
+    //         std::cout << C->board[i][j] << " ";
+    //     }
+    //     std::cout << "\n";
+    // }
+    //UCS(B);
     
     return 0;
 }
